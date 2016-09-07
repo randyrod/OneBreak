41382bc447e9aabdfe4e74fccce2ee7d4f0a8fed
RandyRod <randyro91@gmail.com>
2016-09-07 17:40:21 -0400

    Create ViewModelBase

---
+++ OneBreak/ViewModels/ViewModelBase.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace OneBreak.ViewModels
+{
+    public class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged([CallerMemberName]string propertyChanged = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyChanged));
+        }
+    }
+}
