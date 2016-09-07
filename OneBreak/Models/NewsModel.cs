983f46f7c7a3c20d97a8729a60fba36247f2d62c
RandyRod <randyro91@gmail.com>
2016-09-07 17:41:32 -0400

    Create NewsModel

---
+++ OneBreak/Models/NewsModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OneBreak.Models
+{
+    public class NewsModel
+    {
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public string NewsBody { get; set; }
+
+        public string CoverArtImageUrl { get; set; }
+
+        public string OriginalUrl { get; set; }
+    }
+}
