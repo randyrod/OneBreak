23e91878ebed8e2d93a5f7f4409b6a82af0b2439
RandyRod <randyro91@gmail.com>
2016-09-07 17:35:35 -0400

    Create StringResources Add GPU Rss URI

---
+++ OneBreakUtils/HttpStrings/StringResources.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OneBreakUtils.HttpStrings
+{
+    public class StringResources
+    {
+        public const string GpuRssUrl = "http://feeds.gpupdate.net/en/series/1/formula-1/rss.xml";
+    }
+}
