a9a3e21b8ee8fac8191663e038a2b1384df45e45
RandyRod <randyro91@gmail.com>
2016-09-07 17:37:24 -0400

    Create GPU Rss Model

---
+++ OneBreak/Models/GpuRssModel.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections.Generic;
+using System.Xml.Serialization;
+
+namespace OneBreak.Models
+{
+    [XmlRoot(ElementName = "link", Namespace = "http://www.w3.org/2005/Atom")]
+    public class Link
+    {
+        [XmlAttribute(AttributeName = "rel")]
+        public string Rel { get; set; }
+        [XmlAttribute(AttributeName = "href")]
+        public string Href { get; set; }
+    }
+
+    [XmlRoot(ElementName = "guid")]
+    public class Guid
+    {
+        [XmlAttribute(AttributeName = "isPermaLink")]
+        public string IsPermaLink { get; set; }
+        [XmlText]
+        public string Text { get; set; }
+    }
+
+    [XmlRoot(ElementName = "enclosure")]
+    public class Enclosure
+    {
+        [XmlAttribute(AttributeName = "type")]
+        public string Type { get; set; }
+        [XmlAttribute(AttributeName = "length")]
+        public string Length { get; set; }
+        [XmlAttribute(AttributeName = "url")]
+        public string Url { get; set; }
+    }
+
+    [XmlRoot(ElementName = "item")]
+    public class Item
+    {
+        [XmlElement(ElementName = "title")]
+        public string Title { get; set; }
+        [XmlElement(ElementName = "link")]
+        public string Link2 { get; set; }
+        [XmlElement(ElementName = "description")]
+        public string Description { get; set; }
+        [XmlElement(ElementName = "pubDate")]
+        public string PubDate { get; set; }
+        [XmlElement(ElementName = "guid")]
+        public Guid Guid { get; set; }
+        [XmlElement(ElementName = "enclosure")]
+        public Enclosure Enclosure { get; set; }
+    }
+
+    [XmlRoot(ElementName = "channel")]
+    public class Channel
+    {
+        [XmlElement(ElementName = "title")]
+        public string Title { get; set; }
+        [XmlElement(ElementName = "link")]
+        public List<string> Link { get; set; }
+        [XmlElement(ElementName = "description")]
+        public string Description { get; set; }
+        [XmlElement(ElementName = "lastBuildDate")]
+        public string LastBuildDate { get; set; }
+        [XmlElement(ElementName = "logo", Namespace = "http://www.w3.org/2005/Atom")]
+        public string Logo { get; set; }
+        [XmlElement(ElementName = "item")]
+        public List<Item> Item { get; set; }
+    }
+
+    [XmlRoot(ElementName = "rss")]
+    public class GpuRss
+    {
+        [XmlElement(ElementName = "channel")]
+        public Channel Channel { get; set; }
+        [XmlAttribute(AttributeName = "version")]
+        public string Version { get; set; }
+        [XmlAttribute(AttributeName = "dc", Namespace = "http://www.w3.org/2000/xmlns/")]
+        public string Dc { get; set; }
+        [XmlAttribute(AttributeName = "atom", Namespace = "http://www.w3.org/2000/xmlns/")]
+        public string Atom { get; set; }
+    }
+}
