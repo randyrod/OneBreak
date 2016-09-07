5a780ff80dd165d7154d1df21183933eb0198e1d
RandyRod <randyro91@gmail.com>
2016-09-07 13:49:43 -0400

    Create XML Serialization Helper

---
+++ OneBreakUtils/XmlSerializationHelper.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using System.Xml.Serialization;
+
+namespace OneBreakUtils
+{
+    public class XmlSerializationHelper
+    {
+        public static T DeserializeStringToXmlObject<T>(string xml)
+        {
+            if (string.IsNullOrEmpty(xml)) return default(T);
+
+            try
+            {
+                T result = default(T);
+                using (TextReader reader = new StringReader(xml))
+                {
+                    var serializer = new XmlSerializer(typeof(T));
+                    result = (T)serializer.Deserialize(reader);
+                }
+
+                return result;
+            }
+            catch (Exception)
+            {
+                return default(T);
+            }
+        }
+    }
+}
