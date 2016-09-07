77b7f5f6d644b18a9c0d314c37c6742ff85b5a3f
RandyRod <randyro91@gmail.com>
2016-09-07 17:41:55 -0400

    Create NewsPageViewModel

---
+++ OneBreak/ViewModels/NewsPageViewModel.cs
@@ -0,0 +1,54 @@
+﻿using OneBreak.Helpers;
+using OneBreak.Models;
+using System.Collections.ObjectModel;
+using System.Threading.Tasks;
+
+namespace OneBreak.ViewModels
+{
+    public class NewsPageViewModel : ViewModelBase
+    {
+        private bool _loading;
+        public bool Loading
+        {
+            get { return _loading; }
+            set
+            {
+                if (_loading == value) return;
+                _loading = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private ObservableCollection<NewsModel> _news;
+        public ObservableCollection<NewsModel> News {
+            get { return _news; }
+            set
+            {
+                if (_news == value) return;
+
+                _news = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public NewsPageViewModel()
+        {
+            _news = new ObservableCollection<NewsModel>();
+        }
+
+        public async Task LoadNews()
+        {
+            Loading = true;
+            var result = await NewsRequestHelper.GetGpuNews();
+
+            if (result == null) return;
+
+            foreach (var item in result)
+            {
+                News.Add(item);
+            }
+
+            Loading = false;
+        }
+    }
+}
