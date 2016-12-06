34d5d5f3274a087cdf96d8df9630abe92e321cc3
RandyRod <randyro91@gmail.com>
2016-12-05 22:42:36 -0400

    Create bool to visibility converter

---
+++ OneBreak/Converters/Converters.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Data;
+
+namespace OneBreak.Converters
+{
+    public class BoolToVisibilityConverter : IValueConverter
+    {
+        private bool _inverse;
+
+        public bool Inverse
+        {
+            get { return _inverse; }
+            set { _inverse = value; }
+        }
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            if (!(value is bool)) return false;
+
+            var val = (bool)value;
+
+            if(!Inverse)
+            {
+                var valRet = val ? Visibility.Visible : Visibility.Collapsed;
+                return val ? Visibility.Visible : Visibility.Collapsed;
+            }
+
+            return val ? Visibility.Collapsed : Visibility.Visible;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
