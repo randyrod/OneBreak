d8c27f230a0c576ae0e7d1a9779cbba05c7c18e9
RandyRod <randyro91@gmail.com>
2016-12-05 23:02:43 -0400

    Create News Page

---
+++ OneBreak/Pages/NewsPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using OneBreak.Models;
+using OneBreak.ViewModels;
+using Windows.UI.Xaml.Navigation;
+
+namespace OneBreak.Pages
+{
+    public sealed partial class NewsPage
+    {
+        public NewsViewModel NewsViewModel { get; set; }
+
+        public NewsPage()
+        {
+            this.InitializeComponent();
+        }
+
+        protected async override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+
+            NewsViewModel = App.NewsViewModel;
+
+            await App.NewsViewModel.LoadNews();
+        }
+
+        private void NewsItem_OnClick(object sender, Windows.UI.Xaml.Controls.ItemClickEventArgs e)
+        {
+            var news = e.ClickedItem as NewsModel;
+
+            if (news == null) return;
+
+            var idx = App.NewsViewModel.News.IndexOf(news);
+
+            if (idx == -1) return;
+            NewsDetailFrame.Navigate(typeof(NewsDetailPage), idx);
+        }
+    }
+}
