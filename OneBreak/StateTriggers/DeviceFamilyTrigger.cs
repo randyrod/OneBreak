cee73460acc1fe7d0d42f5c5165f5962a1ce84de
RandyRod <randyro91@gmail.com>
2016-12-14 11:22:08 -0400

    Create device type state trigger

---
+++ OneBreak/StateTriggers/DeviceFamilyTrigger.cs
@@ -0,0 +1,21 @@
+﻿using Windows.UI.Xaml;
+
+namespace OneBreak.StateTriggers
+{
+    public class DeviceFamilyTrigger : StateTriggerBase
+    {
+        private string _currentDeviceFamily, _queriedDeviceFamily;
+
+        public string DeviceFamily
+        {
+            get { return _queriedDeviceFamily; }
+            set
+            {
+                _queriedDeviceFamily = value;
+                _currentDeviceFamily = Windows.System.Profile.AnalyticsInfo.VersionInfo.DeviceFamily;
+
+                SetActive(_queriedDeviceFamily == _currentDeviceFamily);
+            }
+        }
+    }
+}
