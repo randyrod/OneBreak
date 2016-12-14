df408a4f9444e3d551e6b28170c936dcb78c79b8
RandyRod <randyro91@gmail.com>
2016-12-14 17:38:35 -0400

    Create Left nav Item Control

---
+++ OneBreak/Controls/LeftNavItemControl.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace OneBreak.Controls
+{
+    public sealed partial class LeftNavItemControl : UserControl
+    {
+        public DependencyProperty IconDataProperty = DependencyProperty.Register(nameof(IconData), 
+            typeof(string), typeof(LeftNavItemControl), new PropertyMetadata(null));
+        public string IconData
+        {
+            get { return (string)GetValue(IconDataProperty); }
+            set { SetValue(IconDataProperty, value); }
+        }
+
+        public DependencyProperty ItemTextProperty = DependencyProperty.Register(nameof(ItemText), 
+            typeof(string), typeof(LeftNavItemControl), new PropertyMetadata(null));
+        public string ItemText
+        {
+            get { return (string)GetValue(ItemTextProperty); }
+            set { SetValue(ItemTextProperty, value); }
+        }
+
+        public DependencyProperty NavigationPageProperty = DependencyProperty.Register(nameof(NavigationPage), 
+            typeof(Page), typeof(LeftNavItemControl), new PropertyMetadata(null));
+        public Type NavigationPage
+        {
+            get { return (Type)GetValue(NavigationPageProperty); }
+            set { SetValue(NavigationPageProperty, value); }
+        }
+        public LeftNavItemControl()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
