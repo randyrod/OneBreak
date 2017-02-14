3c01962a6227857706043cc1daff39551bed10ee
RandyRod <randyro91@gmail.com>
2017-02-14 11:23:37 -0400

    Include BasePage

---
+++ OneBreak/Pages/BasePage.xaml.cs
@@ -0,0 +1,45 @@
+﻿using Windows.UI.Core;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
+
+namespace OneBreak.Pages
+{
+    public partial class BasePage : Page
+    {
+        private bool _eventsRegistered;
+
+        public BasePage()
+        {
+            this.InitializeComponent();
+        }
+
+        protected virtual void RegisterEvents()
+        {
+            //if (_eventsRegistered) return;
+            //SystemNavigationManager.GetForCurrentView().BackRequested += BasePage_BackRequested;
+            //_eventsRegistered = true;
+        }
+
+        private void BasePage_BackRequested(object sender, BackRequestedEventArgs e)
+        {
+            if (Frame.CanGoBack && !e.Handled)
+            {
+                e.Handled = true;
+                Frame.GoBack();
+            }
+        }
+
+        protected virtual void UnregisterEvents()
+        {
+            //if (!_eventsRegistered) return;
+            //SystemNavigationManager.GetForCurrentView().BackRequested -= BasePage_BackRequested;
+            //_eventsRegistered = false;
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+
+        }
+    }
+}
