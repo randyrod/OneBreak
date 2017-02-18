e12988f7733c33dbee316facc9a3f1434424f618
RandyRod <randyro91@gmail.com>
2017-02-18 14:40:57 -0400

    Create ConnectionHelper

---
+++ OneBreak/Helpers/ConnectionHelper.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Networking.Connectivity;
+
+namespace OneBreak.Helpers
+{
+    public static class ConnectionHelper
+    {
+        public static bool IsConnected
+        {
+            get
+            {
+                if (!NetworkInterface.GetIsNetworkAvailable())
+                {
+                    return false;
+                }
+
+                var profile = NetworkInformation.GetInternetConnectionProfile();
+
+                var level = profile.GetNetworkConnectivityLevel();
+
+                switch (level)
+                {
+                    case NetworkConnectivityLevel.None:
+                    case NetworkConnectivityLevel.LocalAccess:
+                        return false;
+                    case NetworkConnectivityLevel.ConstrainedInternetAccess:
+                    case NetworkConnectivityLevel.InternetAccess:
+                    default:
+                        return true;
+                }
+            }
+        }
+    }
+}
