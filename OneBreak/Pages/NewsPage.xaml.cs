aabd5cc638cde1ef43104346f0704510955282a9
RandyRod <randyro91@gmail.com>
2017-02-18 14:42:40 -0400

    Check for internet connectivity before loading news

--- OneBreak/Pages/NewsPage.xaml.cs
+++ OneBreak/Pages/NewsPage.xaml.cs
@@ -1,5 +1,8 @@
-﻿using OneBreak.Models;
+﻿using OneBreak.Helpers;
+using OneBreak.Models;
 using OneBreak.ViewModels;
+using Windows.Networking.Connectivity;
+using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Navigation;
 
 namespace OneBreak.Pages
@@ -8,21 +11,41 @@ namespace OneBreak.Pages
     {
         public NewsViewModel NewsViewModel { get; set; }
 
+        private bool _onConnectedRegistered;
+
         public NewsPage()
         {
-            this.InitializeComponent();
+            InitializeComponent();
         }
 
-        protected async override void OnNavigatedTo(NavigationEventArgs e)
+        protected override void OnNavigatedTo(NavigationEventArgs e)
         {
             base.OnNavigatedTo(e);
 
             NewsViewModel = App.NewsViewModel;
 
+            if(ConnectionHelper.IsConnected)
+            {
+                LoadNews();
+            }
+            else
+            {
+                RegisterOnConnectedEvent();
+            }
+        }
+
+        protected override void OnNavigatedFrom(NavigationEventArgs e)
+        {
+            base.OnNavigatedFrom(e);
+
+            UnregisterOnConnectedEvent();
+        }
+
+        private async void LoadNews()
+        {
             await App.NewsViewModel.LoadNews();
 
             await App.NewsViewModel.LoadStarredNews();
-
         }
 
         private void NewsItem_OnClick(object sender, Windows.UI.Xaml.Controls.ItemClickEventArgs e)
@@ -34,6 +57,12 @@ namespace OneBreak.Pages
             var idx = App.NewsViewModel.News.IndexOf(news);
 
             if (idx == -1) return;
+
+            if(VisualStateTriggers.CurrentState == MobileState)
+            {
+                Frame.Navigate(typeof(NewsDetailPage), idx);
+                return;
+            }
             NewsDetailFrame.Navigate(typeof(NewsDetailPage), idx);
         }
 
@@ -43,5 +72,35 @@ namespace OneBreak.Pages
 
             NewsViewModel.StarUnstarNews(news);
         }
+
+        #region Events
+        private void RegisterOnConnectedEvent()
+        {
+            if (_onConnectedRegistered) return;
+
+            NetworkInformation.NetworkStatusChanged += NetworkInformation_NetworkStatusChanged;
+            _onConnectedRegistered = true;
+        }
+
+        private void NetworkInformation_NetworkStatusChanged(object sender)
+        {
+            if (!ConnectionHelper.IsConnected) return;
+
+            LoadNews();
+
+            UnregisterOnConnectedEvent();
+
+            _onConnectedRegistered = false;
+        }
+
+        private void UnregisterOnConnectedEvent()
+        {
+            if (!_onConnectedRegistered) return;
+
+            NetworkInformation.NetworkStatusChanged -= NetworkInformation_NetworkStatusChanged;
+
+            _onConnectedRegistered = false;
+        }
+        #endregion
     }
 }
